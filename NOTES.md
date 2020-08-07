8a7ab21949cf8e24efb1c4597104ed7ac5200afe
Kyle Welsby <kyle@mekyle.com>
2020-08-07 17:48:32 +0100

    🚧 hard-coded skeleton of converter
    Here we're quickly spiking how the base functionality HTML display will work before validating functionality with tests.

--- NOTES.md
+++ NOTES.md
@@ -3,3 +3,8 @@
 ## 2020-08-07 17:00:00
 Starting with getting boiler plating a project using Vite.
 You might notice the re-organisation of the challenge source files to `./__DESIGNS__` and `./src/utils/flatten.js` which feel like an ideal place for them in the project right now. 
+
+## 2020-08-07 17:20:00
+The project uses Vue.js with the opinionated Vite builder for simplicity. 
+
+I'm going to get the basic functionality of the project running.  My intent is to use [XState.js](https://xstate.js.org/) and [TailwindCSS](https://tailwindcss.com/).  
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1>Currency Converter</h1>
+  <div>
+    <div id="controls">
+      <div>
+        <label for="baseCurrency">Base</label>
+        <select
+          id="baseCurrency"
+          v-model="baseCurrency"
+          @input="fetchExchangeRates()"
+        >
+          <option v-for="currency of currencies" :key="currency">
+            {{ currency }}
+          </option>
+        </select>
+      </div>
+      <button @click="swap()">Switch</button>
+      <div>
+        <label for="targetCurrency">Target</label>
+        <select id="targetCurrency" v-model="targetCurrency">
+          <option v-for="currency of currencies" :key="currency">
+            {{ currency }}
+          </option>
+        </select>
+      </div>
+      <div>
+        <label for="amount">Amount</label>
+        <input name="amount" type="number" v-model.number="amount" />
+      </div>
+      <div>
+        <span> {{ formattedAmount }} {{ baseCurrency }} = </span>
+        <span> {{ formattedConversion }} {{ targetCurrency }} </span>
+      </div>
+    </div>
+    <div id="main">
+      <div v-for="(value, code) of exchangeRates" :key="code">
+        <div>{{ code }}</div>
+        <div>{{ value }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  import currencies from "./supported_currencies.json";
+  export default {
+    name: "App",
+    components: {},
+    data() {
+      return {
+        currencies,
+        amount: 100,
+        baseCurrency: "INR",
+        targetCurrency: "PLN",
+        exchangeRates: { AUD: 0.01516, PLN: 0.052081 }
+      };
+    },
+    computed: {
+      conversion() {
+        return this.exchangeRates[this.targetCurrency] * this.amount;
+      },
+      formattedAmount() {
+        return new Intl.NumberFormat("en-GB", {
+          style: "currency",
+          currency: this.baseCurrency
+        }).format(this.amount);
+      },
+      formattedConversion() {
+        return new Intl.NumberFormat("en-GB", {
+          style: "currency",
+          currency: this.targetCurrency
+        }).format(this.conversion);
+      }
+    },
+    methods: {
+      fetchExchangeRates() {
+        // TODO: When the base currency is changed, the application should fetch new conversion rates
+      },
+      swap() {
+        // TODO: When the 'switch' button is pressed, the values of the base and target are swapped
+      },
+      calculateValue() {
+        // TODO: When an amount is entered into the input, a converted amount is displayed
+      }
+    }
+  };
 </script>
